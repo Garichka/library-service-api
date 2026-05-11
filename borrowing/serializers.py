eb9baf0e6215bdd89bc17a66781b3e53a58928f8
artem <artem90078@gmail.com>
2026-05-11 14:50:24 +0300

    add telegram notifications for new borrowings

--- borrowing/serializers.py
+++ borrowing/serializers.py
@@ -2,6 +2,7 @@ from rest_framework import serializers
 from book.models import Book
 from borrowing.models import Borrowing
 from book.serializers import BookSerializer
+from borrowing.notifications import send_telegram_notification
 
 
 class BorrowingSerializer(serializers.ModelSerializer):
@@ -28,6 +29,23 @@ class BorrowingSerializer(serializers.ModelSerializer):
         book.inventory -= 1
         book.save()
         return super().create(validated_data)
+
+    def create(self, validated_data):
+        book = validated_data["book"]
+        book.inventory -= 1
+        book.save()
+
+        borrowing = super().create(validated_data)
+
+        message = (
+            f"🚀 <b>New Borrowing Created!</b>\n"
+            f"📖 Book: {book.title}\n"
+            f"👤 User: {borrowing.user.email}\n"
+            f"📅 Return date: {borrowing.expected_return_date}"
+        )
+        send_telegram_notification(message)
+
+        return borrowing
 
 
 class BorrowingListSerializer(BorrowingSerializer):
